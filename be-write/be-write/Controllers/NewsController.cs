74393f5dc43a9b2af174f3d0a0b6c248c61bb94d
vincenzo.petronio <vincenzo.petronio@gmail.com>
2022-01-09 11:58:32 +0100

    ADD be per la parte Command

---
+++ be-write/be-write/Controllers/NewsController.cs
@@ -0,0 +1,78 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace be_write.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly ILogger<NewsController> logger;
+        private readonly IConfiguration configuration;
+
+        public NewsController(ILogger<NewsController> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
+
+        // GET: api/<NewsController>
+        [HttpGet]
+        public async Task<List<News>> Get()
+        {
+            List<News> result = new List<News>();
+
+            await using var connection = new NpgsqlConnection(configuration.GetConnectionString("PostgreSQL"));
+
+            // Esempio con NPSQL base
+            //await using var command = new NpgsqlCommand("SELECT * FROM \"News\"", connection);
+            //await connection.OpenAsync();
+            //await using var reader = await command.ExecuteReaderAsync();
+            //while (reader.Read())
+            //{
+            //    result.Add(new News
+            //    {
+            //        Id = (ulong)reader.GetInt64(0),
+            //        Title = reader.GetString(1),
+            //        Description = reader.GetString(2),
+            //        Timestamp = reader.GetDateTime(3),
+            //        Author = reader.GetString(4)
+            //    });
+            //}
+
+            // Esempio con NPSQL e DAPPER
+            result = (await connection.QueryAsync<News>("SELECT * FROM \"News\"")).ToList();
+
+            return result;
+        }
+
+        // GET api/<NewsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<NewsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NewsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NewsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ be-write/be-write/News.cs
@@ -0,0 +1,18 @@
+﻿namespace be_write
+{
+    public class News
+    {
+        // .NET ulong = Postgres bigserial
+        public ulong Id { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public DateTime Timestamp { get; set; }
+
+        public string? Author { get; set; }
+    }
+}
