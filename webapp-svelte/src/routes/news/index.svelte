ee7d75086d37b5d848bc32e735fa064c0731305b
vincenzo.petronio <vincenzo.petronio@gmail.com>
2022-01-22 19:07:48 +0100

    ADD list of news component

--- webapp-svelte/src/routes/news/index.svelte
+++ webapp-svelte/src/routes/news/index.svelte
@@ -1,11 +1,18 @@
+<script>
+	import NewsList from './newsList.svelte';
+</script>
+
 <svelte:head>
-    <title>Breaking News made with Svelte</title>
-    <!-- <link rel="StyleSheet" href=""> -->
+	<title>Breaking News made with Svelte</title>
+	<!-- <link rel="StyleSheet" href=""> -->
 </svelte:head>
+
 <h1>Breaking News</h1>
 
+<NewsList />
+
 <style>
-    h1 {
-        color: red;
-    }
+	h1 {
+		color: red;
+	}
 </style>
---
+++ webapp-svelte/src/routes/news/newsList.svelte
@@ -0,0 +1,28 @@
+<script>
+	let listOfNews = [
+		{ title: 'aaa', timestamp: '2022-01-01' },
+		{ title: 'bbb', timestamp: '2022-01-02' },
+		{ title: 'ccc', timestamp: '2022-01-03' },
+		{ title: 'ddd', timestamp: '2022-01-04' }
+	];
+
+    let counter = 1;
+
+	function OnRefreshButtonClicked() {
+        // listOfNews.push({title: 'refresh ' + counter, timestamp: new Date()});
+        // listOfNews.push({title: 'eee', timestamp: '2022-01-05'});
+        counter++;
+    }
+</script>
+
+<h2>List of news</h2>
+<ul>
+	{#each listOfNews as { title, timestamp }}
+		<li>
+			{title} [{timestamp}]
+		</li>
+	{/each}
+</ul>
+
+<button on:click={OnRefreshButtonClicked}>Refresh</button>
+<input bind:value="{counter}">
